14227b84a7403ab4e382205aa2a0fe049d96e1e0
haifabenyoussef <116372676+haifabenyoussef@users.noreply.github.com>
2025-05-12 23:28:57 +0100

    Update settings.py

--- farmDjango/farmDjangoProject/settings.py
+++ farmDjango/farmDjangoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +128,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 LOGIN_URL = '/login/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
