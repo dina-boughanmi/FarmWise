8bf79f0864bd25446a7f56edf5e10583df9c69f4
unknown <lynda.mtiri@esprit.tn>
2025-05-13 19:18:51 +0100

    vocal_lynda

---
+++ FarmWiseintegrée/FarmWise-integrée/FarmWise-back-front/farmDjango/main/urls.py
@@ -0,0 +1,41 @@
+# main/urls.py
+from django.urls import path
+from . import views
+from . import auth_views  # import your new auth file
+from .views import PredictPrice,land_view  # Importer correctement la vue
+from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about_view, name='about'),
+    #path('login/', views.login, name='login'),
+    #path('signup/', views.signup_view, name='signup'),  
+    path('contact/', views.contact_view, name='contact'), 
+    path('service/', views.services_view, name='service'), 
+    # path('fertilizer/', views.fertilizer_view, name='fertilizer'), 
+    path('landPrice/', views.land_view, name='landPrice'),         # Affichage HTML
+    path('api/landPrice/', PredictPrice.as_view(), name='predict_land_price'),  # API de p
+    path('yield/', views.yield_view, name='yield'), 
+    path('disease/', views.disease_view, name='disease'),
+    path('cropRotation/', views.cropRotation_view, name='cropRotation'), 
+    path('voluntary/', views.voluntary_view, name='voluntary'), 
+    path('predict/', views.predict_view, name='fertilizer'),    # new form URL
+    path('download-pdf/', views.download_pdf, name='download_pdf'),
+    path('signup/', auth_views.signup_view, name='signup'),
+    path('login/', auth_views.login_view, name='login'),
+    path('logout/', auth_views.logout_view, name='logout'),
+    path('chatbot/', views.agri_chatbot, name='agri_chatbot'),
+    path('profile/', auth_views.profile_view, name='profile'),  # optional, see below
+    path('payment/', auth_views.payment_view, name='payment'),  # ✅ Add this line
+
+
+
+
+
+
+
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
